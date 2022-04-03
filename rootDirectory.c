7cff78418d4967390f231a868bd4f197e1698dc4
Chase Alexander <mr.alexander5132@gmail.com>
2022-04-03 09:43:47 -0700

    Initialized all entries to NULL instead of only the first

--- rootDirectory.c
+++ rootDirectory.c
@@ -8,14 +8,15 @@
 #include "fsLow.h"
 #define ENTRIES_PER_BLOCK 16
 
-typedef struct {
-  int location;//The block number where the start of the file is stored
-  char filename[20];       //The name of the file (provided by creator)
-  unsigned int fileSize;   //Length of file in bytes
-  time_t dateModified;      //Date file was last modified
-  time_t dateCreated;	  //Date file was created
+typedef struct direEntry {
+  int location;           //The block number where the start of the file is stored
+  char filename[20];      //The name of the file (provided by creator)
+  unsigned int fileSize;  //Length of file in bytes
+  time_t dateModified;    //Date file was last modified
+  time_t dateCreated;	    //Date file was created
 } dirEntry;
 
+//Initialize a new directory entry
 dirEntry* dirEntryInit(char filename[20], int location, unsigned int fileSize,
   time_t dateModified, time_t dateCreated) {
 
@@ -30,25 +31,28 @@ dirEntry* dirEntryInit(char filename[20], int location, unsigned int fileSize,
   return entry;
 }
 
-//Hashmap functions
-#define SIZE 1000
+// *************************** Hashmap functions *************************** //
+#define SIZE 1000  //Number of positions in the hashmap
 
+//Node objects are used to populate the hasmap
 typedef struct node {
-  char key[20];
-  dirEntry* value;
-  struct node* next;  //Will point to the next object at that map position
+  char key[20];       //The filename will be used as the key
+  dirEntry* value;    //The directory entry associated with the filename
+  struct node* next;  //points to the next object at that map position
 } node;
 
+//Hashmap object that holds all of the node entries
 typedef struct hashmap {
   node* entries[SIZE];
 } hashmap;
 
+//Initialize a new hashmap
 hashmap* hashmapInit() {
   hashmap* newMap = malloc(sizeof(hashmap));
 
   for (int i = 0; i < SIZE; i++) {
     node* temp = NULL;
-    newMap->entries[0] = temp;
+    newMap->entries[i] = temp;
   }
 
   return newMap;
