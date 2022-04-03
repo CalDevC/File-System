f70ff49da56f294bd7f420a4a8263d9f6d18b949
GurinderS120 <gsingh14@mail.sfsu.edu>
2022-04-03 12:17:24 -0700

    added comments explaining our free space management bit vector

--- fsInit.c
+++ fsInit.c
@@ -1,7 +1,7 @@
 /**************************************************************
 * Class:  CSC-415-0# Fall 2021
-* Names: Patrick Celedio, 
-* Student IDs: 920457223,
+* Names: Patrick Celedio, Gurinder Singh
+* Student IDs: 920457223, 921369355
 * GitHub Name: PatrickCeledio,
 * Group Name: Sudoers
 * Project: Basic File System
@@ -26,25 +26,37 @@
 
 // Magic number for fsInit.c
 #define SIG 12345
-#define FREE_SPACE_START_BLOCK 2
+#define FREE_SPACE_START_BLOCK 1
 
 struct volumeCtrlBlock{
-    long signature;   //Marker left behind that can be checked 
-                        // to know if the disk is setup correctly
-	int blockSize;            //The size of each block in bytes
-	long blockCount;	        //The number of blocks in the file system
-	long numFreeBlocks;      //The number of blocks not in use
+    long signature;           //Marker left behind that can be checked 
+                             // to know if the disk is setup correctly
+	int blockSize;          //The size of each block in bytes
+	long blockCount;       //The number of blocks in the file system
+	long numFreeBlocks;   //The number of blocks not in use
 	int rootDir;		 //Block number where root starts
-	int freeBlockNum;    //To store the block number where our bitmap starts
+	int freeBlockNum;   //To store the block number where our bitmap starts
 } volumeCtrlBlock; 
 
 int initFileSystem (uint64_t numberOfBlocks, uint64_t blockSize)
 	{
-	printf ("Initializing File System with %ld blocks with a block size of %ld\n", numberOfBlocks, blockSize);
+	// printf ("Initializing File System with %ld blocks with a block size of %ld\n", 
+	// numberOfBlocks, blockSize);
+
 	/* TODO: Add any code you need to initialize your file system. */
 	printf("Allocating resources for VCB pointer\n");
 
-	struct volumeCtrlBlock * vcbPtr = malloc(sizeof(volumeCtrlBlock));
+	// We need to allocate memory in block size, because
+	// that's how we read and write data on our volume.
+	struct volumeCtrlBlock * vcbPtr = malloc(blockSize);
+
+	// We need to call LBAread to access our vcb if it exists 
+	// to check if we have already initialized the volume or not
+	int num = LBAread(vcbPtr, 1, 0);
+
+
+	printf("Number of blocks read from the LBA: %d\n", num);
+
 
 	if (vcbPtr->signature == SIG){
 		printf("%d\n", SIG); 
@@ -60,31 +72,21 @@ int initFileSystem (uint64_t numberOfBlocks, uint64_t blockSize)
  		//vcbPtr->rootDir = 
 		vcbPtr->freeBlockNum = FREE_SPACE_START_BLOCK;
 
-		printf("We need to allocate: %ld bytes\n", (numberOfBlocks / 32) + 1);
-		// We need to add one so that we can allocate 2444 bytes
+		// We need at least 19531 bits to represent the corresponding
+		// blocks, or 2442 bytes, and since we will be using ints to 
+		// divide our bitVector into chunks of 32 bits we will need 611
+		// ints, because 611 * 4 = 2444 bytes or 19552 bits. Since int
+		// division truncates the decimal part, we need to add one to
+		// the result.
 		int numOfInts = (numberOfBlocks / 32) + 1;
 
-		// The reason we were getting an error was because here we were
-		// allocating 2444 bytes and using an int pointer (4 bytes) to 
-		// iterate through which means that at every index we have access
-		// to 4 bytes, so in total we have access to 2444 / 4 = 611 blocks 
-		// so we were going way over the allocated 2444 bytes. 2444 bytes
-		// will give us 19,552 bits (since 2444 / 4 = 611 ints and each int 
-		// contains 4 bytes (32bits)) So we can only go from [0 - 610] 
-		// inclusive
-		// int * bitVector = malloc(numOfInts * sizeof(int)); 
-
-         int * bitVector = malloc(5 * blockSize);
+		int * bitVector = malloc(5 * blockSize);
 		
 		// Block 0 is the partition table
 		// Block 1 is the VCB
 
 		// 0 = occupied
 		// 1 = free
-
-		// Initialize bitVector to 0
-		// ERROR: The following (bitVector = 0) was giving the issue:
-		// bitVector = 0;
 
 		// Set first 7 bits to 0 and the rest of
 		// 25 bits of 1st integer to 1
@@ -100,15 +102,14 @@ int initFileSystem (uint64_t numberOfBlocks, uint64_t blockSize)
 			}
 		}
 
-		for (int i = 31; i >= 0; i--) {
-			if (bitVector[0] & (1 << i)) {
-				printf("The value at %dth bit is: %d\n", (31-i) + 1, 1);	
-			} else {
-				printf("The value at %dth bit is: %d\n", (31-i) + 1, 0);
-			}
-		}
+		// The following will help us determine whether a specific
+		// bit is 1 or 0: bitVector[0] & (1 << i), where i represents
+		// the specific bit that you want to check - 1.
 
-		// Set all the bits starting from bit 33 to 1
+		// Set all the bits starting from bit 33 to 1 (free), so
+		// we need to set all the bits in allocated 611
+		// ints to 1. Starting from 2nd int, since we already
+		// handled the 1st int
 		for (int i = 1; i < numOfInts; i++) {
           for (int j = 31; j >= 0; j--) {
 			totalBits++;  
@@ -117,11 +118,9 @@ int initFileSystem (uint64_t numberOfBlocks, uint64_t blockSize)
 		  }
 		}
 
-		printf("Total bits are: %d\n", totalBits);
-        
-
-		printf("Allocating resources for LBAread block 0\n");
-
+		// Write bitVector that allows us to manage our free space
+		// starting at block 1 up to block 6, since our bitVector
+		// takes 5 blocks
 	    int numBlocksWritten = LBAwrite(bitVector, 5, FREE_SPACE_START_BLOCK);
 
 		printf("Number of blocks written to the LBA: %d\n", numBlocksWritten);
